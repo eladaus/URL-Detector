2431b8e928f4699a1d2df8c5b55079e994dcfd8c
Dale Holborow <dale.holborow@f-secure.com>
2019-12-16 16:12:56 +0200

    0002: Initial benchmark project

---
+++ src/urldetector.tests.custom/TestDataHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+namespace urldetector.tests.custom
+{
+	public static class TestDataHelper
+	{
+		public static string LoadSmallHtmlFile()
+		{
+			return File.ReadAllText(@"samples\small.html");
+		}
+
+		public static string LoadLargeHtmlFile()
+		{
+			return File.ReadAllText(@"samples\large.html");
+		}
+
+		public static string LoadMediumHtmlFile()
+		{
+			return File.ReadAllText(@"samples\medium.html");
+		}
+
+		public static string LoadTinyHtmlFile()
+		{
+			return File.ReadAllText(@"samples\tiny.html");
+		}
+	}
+}
