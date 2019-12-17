638f2364b6c7e05491e7e70ceb863982b95d2f0e
Dale Holborow <daleholborow@gmail.com>
2019-12-17 23:39:23 +0200

    WIP

--- src/urldetector.tests.custom/UriSchemeLookupTests.cs
+++ src/urldetector.tests.custom/UriSchemeLookupTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using urldetector.detection;
 using urldetector.eladaus;
 using Xunit;
@@ -22,6 +24,53 @@ namespace urldetector.tests.custom
 				Assert.Equal(2, urls.Count);
 
 			}
+		}
+	}
+
+
+	public class SpanTests
+	{
+		private Memory<char> SomeMemory { get; set; }
+
+		private Span<char> SomeSpan { get; set; }
+
+		[Fact]
+		public void TestIdea()
+		{
+			SomeMemory = new Memory<char>();
+			SomeMemory = "my big test".ToCharArray();
+
+			
+			DoHackery();
+
+			Assert.Equal("my big test::some more text", SomeMemory.ToString());
+
+			DoRemoveHackery();
+		}
+
+		public void DoHackery()
+		{
+			var extraText = "::some more text";
+
+			// First allocate the buffer with the target size
+			char[] buffer = new char[SomeMemory.Length + extraText.Length];
+			// "Convert" it to writable Span<char>
+			var spanBuffer = new Span<char>(buffer);
+
+			// Then copy each span at the right position in the buffer
+			int index = 0;
+			SomeMemory.Span.CopyTo(spanBuffer.Slice(index, SomeMemory.Length));
+			index += SomeMemory.Length;
+
+			extraText.AsSpan().CopyTo(spanBuffer.Slice(index, extraText.Length));
+			index += extraText.Length;
+
+			SomeMemory = spanBuffer.ToArray();
+		}
+
+		private void DoRemoveHackery()
+		{
+			//var removeText
 		}
 	}
 }
